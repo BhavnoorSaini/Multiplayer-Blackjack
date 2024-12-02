c34ab6fbcbf4fca8e3bec109e323ebc4cbbee550
Santiago Ramirez <santirp04@gmail.com>
2024-12-02 16:00:53 -0600

    Moved highscores to index.html

---
+++ Backend/mySocket.js
@@ -0,0 +1,15 @@
+// Blackjack Web Sockets
+
+var myURL = "http://127.0.0.1:3000";
+var socket = io(myURL, {secure: true});
+$.ajax({
+    url: myURL,
+    type: 'GET',
+    success: function (data) {
+        socket.emit('emit_from_here');
+    }
+});
+
+socket.on('broadcast', function (data) {
+    
+});
