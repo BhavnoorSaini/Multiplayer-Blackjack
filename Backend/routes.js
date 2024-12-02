c0aafcb921df6f637a85f5e75efaabc35e6d8494
Santiago Ramirez <santirp04@gmail.com>
2024-12-02 16:58:25 -0600

    Setup routes and express

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,75 +1,40 @@
-//Author: Nnamdi Nwanze
-//Description: This routes module creates custom routes and demonstrates the use of routes using the database manager module
-// Santiago Ramirez
-// CSCE 315
+// Santiago Ramirez & Bhavnoor Saini
+// CSCE 315 - Blackjack Multiplayer Routes
 
-var express = require('express');
+var express = require('express');       // utilize routes
 var router = express.Router();
-//use database manager module
-var mydb = require('./dbmgr.js');
-
-//use the url module
-const url = require('url');
-
-//test variables
-var a = 0;
-var b = 0;
+var mydb = require('./dbmgr.js');       // use database manager module
+const url = require('url');             //use the url module
 
 //Setup database, only need to run this once. Unblock to run once then block this line again
 //mydb.setup();     
 
-
-//Demo / route to print hello
-router.get('/', function (req, res) {
-    res.send("Hello a is " + a + ", b  is " + b);
-});
-
-//Demo / route to print hello
+// username route
+// accepts POST requests with uesrnames and stores in database
 router.post('/username', function (req, res) {
     console.log(req.body);
-    res.send("Hello a is " + a + ", b  is " + b);
 });
 
-//Demo / route to print hello
-router.get('/username', function (req, res) {
-    let myURL = url.parse(req.url, true);
-    console.log(myURL);
-    res.send("Hello we're using a get");
-});
-
-//Demo /p1 route to insert record into the database
-router.get('/p1', function (req, res) {
+// Player 1 route
+// accepts GET req with players name, stats, and status
+router.get('/player1', function (req, res) {
     a++;
     mydb.insertRec({username: 'Player1', 'score': a});
     res.send("Hello Player1! Inserting score b: " + b);
 });
-//Demo /p2 route to insert a different record into the database
-router.get('/p2', function (req, res) {
+
+// Player 2 Route
+router.get('/player2', function (req, res) {
     b++;
     mydb.insertRec({username: 'Player2', 'score': b});
     res.send("Hello Player2! Inserting score a: " + a);
 });
-//Demo /p3 route to find a record in the database
-router.get('/p3', function (req, res) {
+
+// Highscore Route
+// responds with JSON highscore list
+router.get('/highscore', function (req, res) {
     mydb.findRec();
     res.send("Finding 1 record");
-});
-//Demo /p route to find all records in the database
-router.get('/p4', function (req, res) {
-    mydb.findAll(0);
-    res.send("Finding all records");
-});
-//Demo /p5 route to update record the database
-router.get('/p5', function (req, res) {
-    queryData = {username: 'Player1'};
-    upData = {'score': 100};
-    mydb.updateData(queryData, upData);
-    res.send("Updating " + JSON.stringify(queryData) + " score to " + JSON.stringify(upData));
-});
-//Demo /p6 route to delete collection in the database
-router.get('/p6', function (req, res) {
-    mydb.deleteCollection();
-    res.send("Deleted Collection");
 });
 
 module.exports = router;
