eb9869907cf3a31cf9dbf431c31c5fd379baee6b
BhavnoorSaini <bhavnoorsaini@icloud.com>
2024-12-04 18:20:48 -0600

    Remove commented-out code and clean up server.js

--- Backend/server.js
+++ Backend/server.js
@@ -55,39 +55,3 @@ io.on('connection', (socket) => {
         console.log(`A user disconnected. Total users: ${userCount}`);
     });
 });
-
-
-// Extra
-
-// const blackjackAdvice = require('./blackjackadvice'); 
-
-// const server = http.createServer((req, res) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');      // set CORS header
-
-//     const parsedUrl = url.parse(req.url, true);
-//     const userScoreParam = parsedUrl.query.userscore;
-//     const dealerScoreParam = parsedUrl.query.dealerscore;
-//     const outcomeParam = parsedUrl.query.outcome;
-
-//     let myCallback = function(responseData) {       // callback function to send the response
-//         if (responseData.status === 'Success') {
-//             res.statusCode = 200;
-//         } else {
-//             res.statusCode = 400; // Bad Request for invalid outcomes
-//         }
-//         res.setHeader('Content-Type', 'application/json');
-//         res.end(JSON.stringify(responseData));
-//         console.log(`Response sent: ${JSON.stringify(responseData)}`);      // log the response
-//     };
-//     if (outcomeParam) {
-//         // Handle outcome reporting
-//         blackjackAdvice.reportOutcome(outcomeParam, myCallback);
-//     } else if (userScoreParam !== undefined && dealerScoreParam !== undefined) {
-//         // Parse scores as integers
-//         const userScore = parseInt(userScoreParam, 10);
-//         const dealerScore = parseInt(dealerScoreParam, 10);
-
-//         // Handle the advice generation asynchronously
-//         blackjackAdvice.generateAdvice(userScore, dealerScore, myCallback);   
-//     }
-// });
